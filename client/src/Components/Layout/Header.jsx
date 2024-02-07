fed942f28f73d656c91c27f2fddd87e9014362a5
Ajmal <ajmalti2002@gmail.com>
2024-02-07 13:24:00 +0530

    hello

---
+++ client/src/Components/Layout/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='h-50 my-10 flex items-center justify-between lg:h-800 lg:my-10 '>
+        <div>
+            <Link>
+            <img src="https://shopo.quomodothemes.website/assets/images/logo.svg" alt="" />
+            </Link>
+        </div>
+
+        </div>
+  )
+}
+
+export default Header
---
+++ client/src/Pages/HomePage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import Header from '../Components/Layout/Header'
+const HomePage = () => {
+  return (
+    <div><Header/></div>
+  )
+}
+
+export default HomePage
